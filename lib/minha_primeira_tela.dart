e00d761c8760d98fae68b71469e4f82fdce2ce13
Fabio Mendonca <fabio.jose.mendonca@gmail.com>
2023-06-22 08:56:12 -0300

    Novo Widget: Scaffold

---
+++ lib/minha_primeira_tela.dart
@@ -0,0 +1,54 @@
+/*
+Container(
+color: Colors.white,
+child: Column(
+mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+crossAxisAlignment: CrossAxisAlignment.center,
+children: [
+Stack(
+alignment: AlignmentDirectional.center,
+children: [
+Container(color: Colors.red, width: 100, height: 100),
+Container(color: Colors.blue, width: 50, height: 50)
+],
+),
+Stack(alignment: AlignmentDirectional.center, children: [
+Container(color: Colors.blue, width: 100, height: 100),
+Container(color: Colors.red, width: 50, height: 50)
+]),
+Row(
+mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+crossAxisAlignment: CrossAxisAlignment.center,
+children: [
+Container(
+color: Colors.cyan,
+width: 50,
+height: 50,
+),
+Container(
+color: Colors.pinkAccent,
+width: 50,
+height: 50,
+),
+Container(
+color: Colors.purple,
+width: 50,
+height: 50,
+),
+]),
+Container(
+color: Colors.amber,
+height: 30,
+width: 300,
+child:
+Text('Diamante Amarelo',
+textAlign: TextAlign.center,
+style: TextStyle(color: Colors.black, fontSize: 28),),
+),
+ElevatedButton(onPressed: () {
+print('Aperte o botão');
+}, child: Text('Voce apertou o botao'))
+],
+),
+
+)*/
