0122f3aadb0e2accd025747692b6bc9f1194397b
Fabio Mendonca <fabio.jose.mendonca@gmail.com>
2023-06-22 08:29:55 -0300

    aula column

--- lib/main.dart
+++ lib/main.dart
@@ -16,42 +16,58 @@ class MyApp extends StatelessWidget {
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
         ),
-        home: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Stack(
-              alignment: AlignmentDirectional.center,
-              children: [
-                Container(color: Colors.red, width: 100, height: 100),
-                Container(color: Colors.blue, width: 50, height: 50)
-              ],
-            ),
-            Stack(alignment: AlignmentDirectional.center, children: [
-              Container(color: Colors.blue, width: 100, height: 100),
-              Container(color: Colors.red, width: 50, height: 50)
-            ]),
-            Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                crossAxisAlignment: CrossAxisAlignment.center,
+        home: Container(
+          color: Colors.white,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Stack(
+                alignment: AlignmentDirectional.center,
                 children: [
-              Container(
-                color: Colors.cyan,
-                width: 50,
-                height: 50,
+                  Container(color: Colors.red, width: 100, height: 100),
+                  Container(color: Colors.blue, width: 50, height: 50)
+                ],
               ),
+              Stack(alignment: AlignmentDirectional.center, children: [
+                Container(color: Colors.blue, width: 100, height: 100),
+                Container(color: Colors.red, width: 50, height: 50)
+              ]),
+              Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Container(
+                      color: Colors.cyan,
+                      width: 50,
+                      height: 50,
+                    ),
+                    Container(
+                      color: Colors.pinkAccent,
+                      width: 50,
+                      height: 50,
+                    ),
+                    Container(
+                      color: Colors.purple,
+                      width: 50,
+                      height: 50,
+                    ),
+                  ]),
               Container(
-                color: Colors.pinkAccent,
-                width: 50,
-                height: 50,
+                color: Colors.amber,
+                height: 30,
+                width: 300,
+                child:
+                Text('Diamante Amarelo',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(color: Colors.black, fontSize: 28),),
               ),
-              Container(
-                color: Colors.purple,
-                width: 50,
-                height: 50,
-              ),
-            ])
-          ],
+              ElevatedButton(onPressed: () {
+                print('Aperte o botão');
+              }, child: Text('Voce apertou o botao'))
+            ],
+          ),
+
         ));
   }
 }
