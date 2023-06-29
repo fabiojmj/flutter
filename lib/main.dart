1e49733234ab26feaf05d8114d39defc3594395f
Fabio Mendonca <fabio.jose.mendonca@gmail.com>
2023-06-29 08:46:05 -0300

    Implementando o layout

--- lib/main.dart
+++ lib/main.dart
@@ -13,61 +13,40 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
         title: 'Flutter Demo',
         theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-          useMaterial3: true,
-        ),
-        home: Container(
-          color: Colors.white,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Stack(
-                alignment: AlignmentDirectional.center,
-                children: [
-                  Container(color: Colors.red, width: 100, height: 100),
-                  Container(color: Colors.blue, width: 50, height: 50)
-                ],
-              ),
-              Stack(alignment: AlignmentDirectional.center, children: [
-                Container(color: Colors.blue, width: 100, height: 100),
-                Container(color: Colors.red, width: 50, height: 50)
-              ]),
-              Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Container(
-                      color: Colors.cyan,
-                      width: 50,
-                      height: 50,
-                    ),
-                    Container(
-                      color: Colors.pinkAccent,
-                      width: 50,
-                      height: 50,
-                    ),
-                    Container(
-                      color: Colors.purple,
-                      width: 50,
-                      height: 50,
-                    ),
-                  ]),
-              Container(
-                color: Colors.amber,
-                height: 30,
-                width: 300,
-                child:
-                Text('Diamante Amarelo',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(color: Colors.black, fontSize: 28),),
-              ),
-              ElevatedButton(onPressed: () {
-                print('Aperte o botão');
-              }, child: Text('Voce apertou o botao'))
-            ],
+            //colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+            //useMaterial3: true,
+            ),
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text("Tarefas"),
           ),
-
+          body: Container(
+              child: Stack(children: [
+            Container(
+              color: Colors.blue,
+              height: 140,
+            ),
+            Container(
+                color: Colors.white,
+                height: 100,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                  Container(
+                    color: Colors.black26,
+                    width: 72,
+                    height: 100,
+                  ),
+                  Text('Aprender Flutter'),
+                  ElevatedButton(onPressed: (){}, child: Icon(Icons.arrow_drop_up))
+                ])
+            )
+          ])
+              //color: Colors.black,
+              ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {},
+          ),
         ));
   }
 }
